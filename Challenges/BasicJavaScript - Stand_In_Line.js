f0a44e751e543f10d1a1cdd33e82c94652d8a4ac
Zahed Shareef <realzahed@gmail.com>
2017-08-20 14:36:51 +0530

    cleanup

--- Challenges/BasicJavaScript - Stand_In_Line.js
+++ Challenges/BasicJavaScript - Stand_In_Line.js
@@ -1,15 +1,14 @@
-
 function nextInLine(arr, item) {
   // Your code here
   arr.push(item);
   var removed = arr.shift();
-  return removed;  // Change this line
+  return removed; // Change this line
 }
 
 // Test Setup
-var testArr = [1,2,3,4,5];
+var testArr = [1, 2, 3, 4, 5];
 
 // Display Code
 console.log("Before: " + JSON.stringify(testArr));
-console.log(nextInLine([2],1)); // Modify this line to test
+console.log(nextInLine([2], 1)); // Modify this line to test
 console.log("After: " + JSON.stringify(testArr));
